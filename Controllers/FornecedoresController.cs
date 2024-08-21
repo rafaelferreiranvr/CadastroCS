e0f895ed15aa9083f455008a318ea93b23b0049d
Rafael Neviro <rafaelneviro@outlook.com>
2024-08-21 04:58:38 -0300

    ...

--- Controllers/FornecedoresController.cs
+++ Controllers/FornecedoresController.cs
@@ -44,7 +44,7 @@ namespace CadastroCS.Controllers
         }
 
         // GET: Fornecedors/Create
-        public IActionResult Create()
+        public IActionResult Cadastro()
         {
             return View();
         }
@@ -54,7 +54,7 @@ namespace CadastroCS.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Cnpj,Segmento,Cep,Endereco,Perfil")] Fornecedor fornecedor)
+        public async Task<IActionResult> Cadastro([Bind("Id,Name,Cnpj,Segmento,Cep,Endereco,Perfil")] Fornecedor fornecedor)
         {
             if (ModelState.IsValid)
             {
@@ -79,79 +79,6 @@ namespace CadastroCS.Controllers
                 return NotFound();
             }
             return View(fornecedor);
-        }
-
-        // POST: Fornecedors/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Cnpj,Segmento,Cep,Endereco,Perfil")] Fornecedor fornecedor)
-        {
-            if (id != fornecedor.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(fornecedor);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!FornecedorExists(fornecedor.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(fornecedor);
-        }
-
-        // GET: Fornecedors/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var fornecedor = await _context.Fornecedor
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (fornecedor == null)
-            {
-                return NotFound();
-            }
-
-            return View(fornecedor);
-        }
-
-        // POST: Fornecedors/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var fornecedor = await _context.Fornecedor.FindAsync(id);
-            if (fornecedor != null)
-            {
-                _context.Fornecedor.Remove(fornecedor);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool FornecedorExists(int id)
-        {
-            return _context.Fornecedor.Any(e => e.Id == id);
         }
     }
 }

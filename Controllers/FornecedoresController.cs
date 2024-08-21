31e41d57d45d49940ee5e19983d24aa385cfa9f3
Rafael Neviro <rafaelneviro@outlook.com>
2024-08-21 17:50:37 -0300

    ...

--- Controllers/FornecedoresController.cs
+++ Controllers/FornecedoresController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using CadastroCS.Data;
 using CadastroCS.Models;
 using NuGet.Common;
+using Microsoft.AspNetCore.Http;
 
 namespace CadastroCS.Controllers
 {
@@ -59,9 +60,12 @@ namespace CadastroCS.Controllers
                 await _context.SaveChangesAsync();
 
                 var files = Request.Form.Files;
-                if (files.Count > 0){ SetImage(files[0], fornecedor); }
+                if (files.Count > 0){ 
+                    await SetImage(files[0], fornecedor); 
+                }
 
                 return RedirectToAction(nameof(Index));
+
             }
             return View(fornecedor);
         }
@@ -82,13 +86,74 @@ namespace CadastroCS.Controllers
             return View(fornecedor);
         }
 
-        public void SetImage(IFormFile file, Fornecedor fornecedor)
+        public async Task<IActionResult> SetImage(IFormFile file, Fornecedor fornecedor)
         {
-            path = "~/profiles/" + fornecedor.Id.ToString() + ".png";
-            var filename = file.FileName;
-            var path = Path.Combine("~/profiles", string(fornecedor.Id);
-            Console.Write("");
+            string path = "./wwwroot/profiles/" + fornecedor.Id.ToString() + ".png";
 
+            using (var stream = System.IO.File.Create(path))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            fornecedor.Foto = path;
+            await _context.SaveChangesAsync();
+
+            return Ok("Imagem foi carregada com sucesso.");
+
+        }
+
+        public async Task<IActionResult> GetImage(int id)
+        {
+            string path = "./wwwroot/profiles/" + id.ToString() + ".png";
+
+            if (System.IO.File.Exists(path))
+            {
+                byte[] fileBytes = await System.IO.File.ReadAllBytesAsync(path);
+                return File(fileBytes, "image/png");
+
+            } else
+            {
+                byte[] fileBytes = await System.IO.File.ReadAllBytesAsync("./wwwroot/profiles/default.png");
+                return File(fileBytes, "image/png");
+
+            }
+
+        }
+
+        // GET: FornecedoresController1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _context.Fornecedor
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedor);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var fornecedor = await _context.Fornecedor.FindAsync(id);
+            if (fornecedor != null)
+            {
+                _context.Fornecedor.Remove(fornecedor);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool FornecedorExists(int id)
+        {
+            return _context.Fornecedor.Any(e => e.Id == id);
         }
 
     }
